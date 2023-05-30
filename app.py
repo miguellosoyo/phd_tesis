25da4c1ea65b0961e563628551618b833262653f
Miguel Granados <44987101+miguellosoyo@users.noreply.github.com>
2023-05-30 16:57:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ affections = pd.read_csv('https://raw.githubusercontent.com/miguellosoyo/phd_tes
 # Seleccionar las primeras 12 semanas
 data = data.iloc[:, :15]
 affections = affections.iloc[:, :15]
+st.dataframe(affections)
 
 # Integrar a la barra lateral la selección de concesionarios y tipo de reporte
 # with st.sidebar:
