5e810c0932775f61eb9c8e8377b943e2499d8d32
Miguel Granados <44987101+miguellosoyo@users.noreply.github.com>
2023-05-30 17:00:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -73,13 +73,7 @@ sections = pd.DataFrame([
                         {'Semana':8, 'Cigarros':y_lim[-1]-9, 'Etiqueta':''},
                         {'Semana':2, 'Cigarros':y_lim[-1]-9, 'Etiqueta':'LB'},
                         {'Semana':7, 'Cigarros':y_lim[-1]-9, 'Etiqueta':'Tratamiento'},
-                        {'Semana':22, 'Cigarros':y_lim[-1]-9, 'Etiqueta':'Seguimiento'},
-                        ])
-sections = pd.DataFrame([
-                        {'Semana':4, 'Cigarros':y_lim[-1]-9, 'Etiqueta':''},
-                        {'Semana':8, 'Cigarros':y_lim[-1]-9, 'Etiqueta':''},
-                        {'Semana':2, 'Cigarros':y_lim[-1]-9, 'Etiqueta':'LB'},
-                        {'Semana':7, 'Cigarros':y_lim[-1]-9, 'Etiqueta':'Tratamiento'},
+                        {'Semana':9, 'Cigarros':y_lim[-1]-9, 'Etiqueta':'Seguimiento'},
                         ])
 
 # Crear la gráfica de líneas
