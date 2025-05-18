b84a6f300cd5c290f01160267c4f0688dbcf772e
Miguel Granados <44987101+miguellosoyo@users.noreply.github.com>
2025-05-17 23:27:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ df['Semana'] = df['Semana'].astype(int)
 
 # Seleccionar hasta el 15vo usuario
 # df = df[df['ID'].astype(int)<=15].copy()
-data_users = data_users[data_users['ID'].astype(int)<=15].copy()
+# data_users = data_users[data_users['ID'].astype(int)<=15].copy()
 
 # Filtrar información por paciente para los casos de afecto
 df_affections = affections[affections['Usuario']==name].melt(id_vars=['ID', 'Nombre', 'Usuario', 'Tipo'], var_name='Semana', value_name='Nivel de Afecto').copy()
